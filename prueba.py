5e72abda3671bae6590dca4dfe3e4677e64b3fac
ElianPerez <49233875+ElianPerez@users.noreply.github.com>
2019-04-04 20:57:25 -0600

    Arreglos del archivo .py
    Se agregaron mejoras a la progra

--- prueba.py
+++ prueba.py
@@ -1,13 +1,11 @@
 x = 3.1
 y = x//1
+
 if (x-y ==0):
     
-    
    print("Es entero")
-    
-    
-    
+   print("Esta papaya debe de funcionar")
+   print("Santo Santo Santo")
+
 else:
     print("es flotante")
-    
-    
